7f0fd5f5cab73679867234a77f08cbe7afc8d1d6
jdvalera <johnlemuelvalera@gmail.com>
2021-07-17 15:48:47 -0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,97 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="ctr">
+    <questions v-if="questionsAnswered < questions.length" />
+    <result v-else />
+    <button type="button" class="reset-btn">Reset</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Questions from './components/Questions.vue';
+import Result from "./components/Result.vue";
 
 export default {
+  components: { Questions, Result },
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      questionsAnswered: 0,
+      questions: [
+          {
+              q: 'What is 2 + 2?', 
+              answers: [
+                  {
+                      text: '4',
+                      is_correct: true
+                  },
+                  {
+                      text: '3',
+                      is_correct: false 
+                  },
+                  {
+                      text: 'Fish',
+                      is_correct: false 
+                  },
+                  {
+                      text: '5',
+                      is_correct: false 
+                  }
+              ] 
+          },
+          { 
+              q: 'How many letters are in the word "Banana"?', 
+              answers: [
+                  {
+                      text: '5',
+                      is_correct: false
+                  },
+                  {
+                      text: '7',
+                      is_correct: false 
+                  },
+                  {
+                      text: '6',
+                      is_correct: true 
+                  },
+                  {
+                      text: '12',
+                      is_correct: false 
+                  }
+              ] 
+          },
+          { 
+              q: 'Find the missing letter: C_ke', 
+              answers: [
+                  {
+                      text: 'e',
+                      is_correct: false
+                  },
+                  {
+                      text: 'a',
+                      is_correct: true 
+                  },
+                  {
+                      text: 'i',
+                      is_correct: false 
+                  }
+              ] 
+          },
+      ],
+      results: [
+          {
+              min: 0,
+              max: 2,
+              title: "Try again!",
+              desc: "Do a little more studying and you may succeed!"
+          },
+          {
+              min: 3,
+              max: 3,
+              title: "Wow, you're a genius!",
+              desc: "Studying has definitely paid off for you!"
+          }
+      ]
+}
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
